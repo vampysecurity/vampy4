c038727efd7243828d9dd9eea2a614bcee49164b
Vampy Security <96365539+vampysecurity@users.noreply.github.com>
2021-12-19 21:53:43 -0600

    Add files via upload

---
+++ vampy4.py
@@ -0,0 +1,89 @@
+print("uso:           ")
+print("solamente escriba la letra correspondiente a el color ")
+print("una ves traducido debera otras ves escribir python2 colors.py   ")
+print("")
+print("")
+while True:
+        print ("1.- negro \n")
+        print ("2.- blanco \n")
+        print ("3.- gris \n")
+        print ("4.- verde \n")
+        print ("5.- azul \n")
+        print ("6.- anaranjado \n")
+        print ("7.- amarillo \n")
+        print ("8.- cafe o marron \n")
+        print ("9.- morado o purpura \n")
+        print ("10.- color oro \n")
+        print ("11.- color plata \n")
+        print ("12.- rojo \n")
+        print ("13.- rosa \n")
+        print ("14.- transparente \n")
+        print ("z.- exit o salir \n")
+        print("elije una opcion del menu \n")
+        opcion=str(input("selecciona una color para proseguir  \n"))
+        if opcion == "1" :
+                print("-----------------------")
+                print("\n se dice black \n")
+                print("-----------------------")
+        elif opcion == "2":
+                print("-----------------------")
+                print("\n se dice white \n")
+                print("-----------------------")
+        elif opcion == "3":
+                print("-----------------------")
+                print("\n se dice gray \n")
+                print("-----------------------")
+        elif opcion == "4":
+                print("-----------------------")
+                print("\n se dice green \n")
+                print("-----------------------")
+        elif opcion == "5":
+                print("-----------------------")
+                print("\n se dice blue \n")
+                print("-----------------------")
+        elif opcion == "6":
+                print("-----------------------")
+                print("\n se dice orange \n")
+                print("-----------------------")
+        elif opcion == "7":
+                print("-----------------------")
+                print("\n se dice yellow \n")
+                print("-----------------------")
+        elif opcion == "8":
+                print("-----------------------")
+                print("\n se dice brown \n")
+                print("-----------------------")
+        elif opcion == "9":
+                print("-----------------------")
+                print("\n se dice purple \n")
+                print("-----------------------")
+        elif opcion == "10":
+                print("-----------------------")
+                print("\n se dice gold \n")
+                print("-----------------------")
+        elif opcion == "11":
+                print("-----------------------")
+                print("\n se dice silver \n")
+                print("-----------------------")
+        elif opcion == "12":
+                print("-----------------------")
+                print("\n se dice red \n")
+                print("-----------------------")
+        elif opcion == "13":
+                print("-----------------------")
+                print("\n se dice pink \n")
+                print("-----------------------")
+        elif opcion == "14":
+                print("-----------------------")
+                print("\n se dice transparent \n")
+                print("-----------------------")
+        elif opcion == "z":
+                print("--------------------------------------------")
+                print("gracias por utilizar el script educativo")
+                print("--------------------------------------------")
+                break
+
+        else:
+                print("el color seleccionado no existe o no fue tomado en cuenta")
+                print("si desea que el color seleccionado sea agregado comuniquese")
+                print("a telegram con @vampysecurity")
